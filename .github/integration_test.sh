0e515f7b97d005dd79a6a4bffbe9ec49fc060cd8
root <root@DESKTOP-NAN6QAO-wsl.localdomain>
2023-03-09 20:40:31 +0900

    シェルを更新

--- .github/integration_test.sh
+++ .github/integration_test.sh
@@ -7,5 +7,22 @@
 
 echo "Read the integration_test.sh"
 cd ./sample_app/selenium
-npm run dev
+
+# Start Next.js application in the background
+nohup next dev > /dev/null 2>&1 &
+echo "Waiting for server to start..."
+
+# Wait until server is ready
+until $(curl --output /dev/null --silent --head --fail http://localhost:3000); do
+  printf '.'
+  sleep 1
+done
+
+echo "Server is ready!"
+
+# Run Selenium tests
 node sample_test.ts
+
+# Stop Next.js application
+kill $(lsof -t -i :3000 -sTCP:LISTEN) > /dev/null 2>&1
+echo "Server stopped."
