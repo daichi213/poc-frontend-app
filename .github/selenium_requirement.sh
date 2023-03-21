6ea069ed0ca78b5c9f07586d35ea1a817129514a
root <root@DESKTOP-NAN6QAO-wsl.localdomain>
2023-03-22 01:23:40 +0900

    実験的にワークフロー修正

--- .github/selenium_requirement.sh
+++ .github/selenium_requirement.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 # xmllint利用のため
-apt-get -y install libxml2-utils
-sudo apt-get install -y wget unzip
+sudo apt -y install libxml2-utils
+sudo apt install -y wget unzip
 
 # sudoで実行すること
 # google chromeのインストーラを作成
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-
+sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 # パッケージリストの更新とGoogle Chromeのインストール
-sudo apt-get install -y google-chrome-stable
+sudo apt install -y google-chrome-stable
 
 # chromeのバージョンを取得
 chrome_v=$(google-chrome --version | cut -d " " -f 3 | cut -d "." -f 1)
@@ -20,7 +19,7 @@ chrome_chrome_ver_list=`xmllint --format /tmp/chrome.v | grep Key | grep ${chrom
 chrome_driver_ver=`echo ${chrome_chrome_ver_list} | cut -d" " -f1`
 
 # google-chromeと同じバージョンをwget
-wget https://chromedriver.storage.googleapis.com/${chrome_driver_ver}/chromedriver_linux64.zip -P /tmp
-unzip /tmp/chromedriver_linux64.zip -d /usr/local/bin
+sudo wget https://chromedriver.storage.googleapis.com/${chrome_driver_ver}/chromedriver_linux64.zip -P /tmp
+sudo unzip /tmp/chromedriver_linux64.zip -d /usr/local/bin
 sudo chmod 755 /usr/local/bin/chromedriver
-rm -f /tmp/chromedriver_linux64.zip
+sudo rm -f /tmp/chromedriver_linux64.zip
