21c70dcf822144fe5eca0102fd4725a419c4d2ec
Fang-Siroh <59591435+Fang-Siroh@users.noreply.github.com>
2023-04-14 22:23:56 +0900

    Feature/techblog-61 (#3)
    Seleniumの自動テスト実装

--- .github/integration_test.sh
+++ .github/integration_test.sh
@@ -6,3 +6,14 @@
 ####################################################
 
 echo "Read the integration_test.sh"
+cd ./sample_app
+
+# Start Next.js application in the background
+yarn run build
+yarn run start > /dev/null 2>&1 &
+echo "Next.js is running..."
+
+echo "Run Selenium tests"
+cd ./selenium
+node sample_test.cjs
+echo "Finished Selenium tests"
