5b67cb85e0dcb4942f677a313bfe15a6811f78f9
daichi213 <bestabokadon910@gmail.com>
2023-06-03 14:38:47 +0900

    Buildジョブ・IntegrationTestジョブを分割

--- .github/build_and_test.sh
+++ .github/build_and_test.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env sh
 
-####################################################
+##################  Description  ###################
 # AppSrc内でビルドする処理を記述するためのシェル
-# シェルの実行はレポジトリのルートディレクトリになるため、package.jsonが存在するディレクトリへ移動してからyarnコマンドを実行する
+
+# このシェルで実施が必要な処理
+
+# 1. シェルの実行はレポジトリのルートディレクトリになるため、package.jsonが存在するディレクトリへ移動してからyarnコマンドを実行する
+# 2. Github PagesへのデプロイおよびIntegrationTestは別ジョブで実行するため、一時的にGithub Artifactへ格納する必要がある。ここにアップロードするために、UPLOAD_FILES_PATHへビルド成果物を格納する。
+####################################################
+
+#####################  引数  #######################
+# Github PagesへのデプロイおよびIntegrationTestを実施するビルド成果物（index.html , main.js....）を格納するためのディレクトリ
+UPLOAD_FILES_PATH=$1
 ####################################################
 
 ####################################################
@@ -33,11 +42,11 @@ yarn run build
 ####################################################
 
 # Start Next.js application in the background
-echo "########## Integration Test ##########"
-yarn run start > /dev/null 2>&1 &
-echo "Next.js is running..."
+# echo "########## Integration Test ##########"
+# yarn run start > /dev/null 2>&1 &
+# echo "Next.js is running..."
 
-echo "Run Selenium tests"
-cd ./selenium
-node sample_test.cjs
-echo "Finished Selenium tests"
+# echo "Run Selenium tests"
+# cd ./selenium
+# node sample_test.cjs
+# echo "Finished Selenium tests"
---
+++ .github/integration_test.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+##################  Description  ###################
+# Seleniumを使用した統合テスト
+####################################################
+
+#####################  引数  #######################
+# ビルド資材が格納されているディレクトリのパス
+BUILD_FILES_PATH=$1
+####################################################
+
+# Start Next.js application in the background
+echo "########## Integration Test ##########"
+yarn run start > /dev/null 2>&1 &
+echo "Next.js is running..."
+
+echo "Run Selenium tests"
+cd ./selenium
+node sample_test.cjs
+echo "Finished Selenium tests"
